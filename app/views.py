7154ee534d42c1b2d18e17e7dc61302dabd99b38
Rachel Shorey <rshorey@gmail.com>
2015-10-31 14:27:20 -0400

    simple dumb templates

--- app/views.py
+++ app/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from django.template import RequestContext, loader
+from app.models import *
 
 def index(request):
-    return HttpResponse("Hello, world. This is the front page of Diamond's List.")
+    listings = Listing.objects.all()
+    context = {'listings': listings}
+    return render(request, 'app/index.html', context)
 
 def user(request, user_id):
-    return HttpResponse("This is the page for user {}".format(user_id))
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'app/user.html', {'user': user})
 
 def listing(request, listing_id):
-    return HttpResponse("This is the page for listing {}".format(listing_id))
+    listing = get_object_or_404(Listing, pk=listing_id)
+    return render(request, 'app/listing.html', {'listing': listing})
