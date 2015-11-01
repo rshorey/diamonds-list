cda310338a1053a73de6311c477b9ce676ee2911
Rachel Shorey <rshorey@gmail.com>
2015-11-01 09:33:29 -0500

    static

--- diamondslist/settings.py
+++ diamondslist/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 
