d367f1a16fa18781f8d9928aa081dd1bc1068b22
Rachel Shorey <rshorey@gmail.com>
2015-11-01 08:05:45 -0500

    trying with dj_database_url '

--- diamondslist/settings.py
+++ diamondslist/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
  }
 }
 
-#import dj_database_url
-#DATABASES['default'] = dj_database_url.config()
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
